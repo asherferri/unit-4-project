ceb97a5f523a53986f84d6b6773819a2624e48cc
Asher <asherferri85@gmail.com>
2020-10-06 14:51:01 -0400

    send extra switch to it's own module switches.js

--- switchOfEol/switches.js
+++ switchOfEol/switches.js
@@ -1,3 +1,27 @@
+let socket2 = io.connect() 
+gpioON2 = () => {
+    socket2.emit("pwm14", 1) 
+}
+gpioOFF2 = () => {
+    socket2.emit("pwm14", 0) 
+}
+
+let socket3 = io.connect()
+gpioON3 = () => {
+    socket3.emit("pwm7", 1) 
+}
+gpioOFF3 = () => {
+    socket3.emit("pwm7", 0) 
+}
+
+// let socketA = io.connect() 
+// gpioONA = () => {
+//     socketA.emit("stateA", 1) 
+// }
+// gpioOFFA = () => {
+//     socketA.emit("stateA", 0) 
+// }
+
 changeLed = () => {
     let clicked = document.getElementById('switchOnOff')
         let socket3 = io.connect()
